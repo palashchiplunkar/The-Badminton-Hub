62079b2adb65cb1ffb28c22f2eff8c5c70ccdf62
Palash <palashchiplunkar51@gmail.com>
2021-12-11 20:06:22 +0530

    Added Main screen

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+app.set("views", __dirname + "/screens/views");
+app.use(express.static(__dirname + "/assets"));
+app.use(express.static(__dirname + "/screens/css"));
+app.set("view-engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("main.ejs");
+});
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Wold!");
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
 app.listen(port, () => {
